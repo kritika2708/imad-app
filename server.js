733bd22fa2738c81fa274f95a182a50c812fca62
kritika2708 <30926139+kritika2708@users.noreply.github.com>
2017-08-21 13:54:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,16 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+ app.get('article-one', function(req,res){
+     res.send('article one will be displayed');
+ });
+ app.get('article-two', function(req,res){
+     res.send('article two will be displayed');
+ });
+ app.get('article-three', function(req,res){
+     res.send('article three will be displayed');
+ });
+ 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
